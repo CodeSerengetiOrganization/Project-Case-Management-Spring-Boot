6bb68f0dca7068cb1f729034ddfdcef1ba3db87f
ProgrammerBSeren <programmerb.serengeti@gmail.com>
2024-09-14 18:03:24 -0400

    ticket 14: added Kafka Consumer related dependency and functianal code:properties file,Config and Property java class, MessageListener for CaseNew.

---
+++ src/main/java/com/mytech/casemanagement/kafka/consumer/MessageListenerCaseNew.java
@@ -0,0 +1,34 @@
+package com.mytech.casemanagement.kafka.consumer;
+
+
+import io.swagger.client.model.CaseNew;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListenerCaseNew {
+
+    //todo: to make topics as "${kafka.topic} in common config"
+/*    @KafkaListener(topics="test-topic", groupId = "case-consumer-1")
+    public void listenerCaseNew(ConsumerRecord<String, CaseNew> record){
+        //code to handle the record
+        System.out.println("record: "+record.toString());
+    }*/
+
+    @KafkaListener(topics="test-topic", groupId = "case-consumer-1")
+    public void consume(CaseNew message){
+        System.out.println("Received message: " + message);
+    }
+}
+
+/*
+* @Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "your_topic", groupId = "group_id")
+    public void consume(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
+*
+* */
