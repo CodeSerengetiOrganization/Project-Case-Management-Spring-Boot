4bc35b8a0d3e559f9939e984e168319aa5a88687
ProgrammerBSeren <programmerb.serengeti@gmail.com>
2024-10-04 10:25:11 -0400

    ticket 35: Added unit test code for  updateCase() in service layer, fix some logic bug.

--- src/main/java/com/mytech/casemanagement/service/CaseServiceNew.java
+++ src/main/java/com/mytech/casemanagement/service/CaseServiceNew.java
@@ -1,10 +1,9 @@
 package com.mytech.casemanagement.service;
 
-import com.mytech.casemanagement.entity.Case;
 import com.mytech.casemanagement.entity.CaseNew;
 import com.mytech.casemanagement.entity.CaseStatusEnum;
 import com.mytech.casemanagement.entity.CaseTypeEnum;
-import com.mytech.casemanagement.repository.CaseRepository;
+import com.mytech.casemanagement.exception.CaseResourceNotFoundException;
 import com.mytech.casemanagement.repository.CaseRepositoryNew;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +13,16 @@ import java.util.Optional;
 @Service
 public class CaseServiceNew {
 
-    @Autowired
+/*    @Autowired
+    private CaseRepositoryNew caseRepository;*/
     private CaseRepositoryNew caseRepository;
+
+    @Autowired
+    public CaseServiceNew(CaseRepositoryNew repository){
+        this.caseRepository = repository;
+    }
+
+
 
 /*
     public Optional<Case> getCaseByCaseId(int caseId) {
@@ -40,22 +47,24 @@ public class CaseServiceNew {
         Optional<Integer> caseId = Optional
                 .ofNullable(caseNew)
                 .map(CaseNew::getCaseId);
-        if (caseId.isPresent() && caseId.get()>0){  //use caseId.get()>0 to make sure casId exist in payload, as when caseId is missing in payload, the CaseNew entity will initialized caseId with 0;
+        if (caseId.isPresent()){  //use caseId.get()>0 to make sure casId exist in payload, as when caseId is missing in payload, the CaseNew entity will initialized caseId with 0;
+            if (caseId.get() ==0){  //caseId is primitive type int,so it will be 0 if try to get()
+                throw new CaseResourceNotFoundException(String.format("caseID not existing in request body"));
+            }
+            if(caseId.get()<0){
+                throw new IllegalArgumentException(String.format("caseId [%d] format is less than 0",caseId.get()));
+            }
             Optional<CaseNew> retrievedCase = caseRepository.findByCaseId(caseId.get());
             if (retrievedCase.isPresent()){
                 CaseNew updatedCaseNew = caseRepository.save(caseNew);
                 return updatedCaseNew;
             }else{
-                throw new RuntimeException("caseID not existing in database");
+                throw new CaseResourceNotFoundException(String. format("caseID [%d] not existing in database",caseId.get()));
             }
 
         }
-        throw new RuntimeException("caseID not existing in request body");
+        throw new CaseResourceNotFoundException(String.format("caseID not existing in request body"));
 
- /*       else{
-
-        }
-        return updatedCaseNew;*/
     }
 /*
 * to covert a CaseNew from Kafka message to a CaseNew Entity
