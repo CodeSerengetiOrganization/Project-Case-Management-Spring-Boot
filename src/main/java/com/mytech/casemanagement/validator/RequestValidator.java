2bfa90cecc17bb3b0562efcc46e20ad348cd1db2
ProgrammerBSeren <programmerb.serengeti@gmail.com>
2025-01-21 18:25:31 -0500

    ticket 53: Added Unit Test for RequestValidator. Modified annotation for RequestValidator,replace @Component with @Service.

--- src/main/java/com/mytech/casemanagement/validator/RequestValidator.java
+++ src/main/java/com/mytech/casemanagement/validator/RequestValidator.java
@@ -1,8 +1,8 @@
 package com.mytech.casemanagement.validator;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class RequestValidator {
     public void validateRequestPayload(String requestStr){
         if(requestStr == null || requestStr.isBlank()){
